cc4c3911655ca361501599c995742ed4349a13df
itobek00 <100638298+itobek00@users.noreply.github.com>
2022-03-08 01:52:27 +0100

    Add files via upload

---
+++ projekt/app/calc.php
@@ -0,0 +1,72 @@
+<?php
+// KONTROLER strony kalkulatora
+require_once dirname(__FILE__).'/../config.php';
+
+// W kontrolerze niczego nie wysyła się do klienta.
+// Wysłaniem odpowiedzi zajmie się odpowiedni widok.
+// Parametry do widoku przekazujemy przez zmienne.
+
+// 1. pobranie parametrów
+
+$amount = $_REQUEST ['kwota'];
+$time = $_REQUEST ['czas'];
+$percentage = $_REQUEST ['oprocentowanie'];
+
+// 2. walidacja parametrów z przygotowaniem zmiennych dla widoku
+
+// sprawdzenie, czy parametry zostały przekazane
+if ( ! (isset($amount) && isset($time) && isset($percentage))) {
+	//sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+	$info [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
+}
+
+// sprawdzenie, czy potrzebne wartości zostały przekazane
+if ( $amount == "") {
+	$info [] = 'Nie podano kwoty';
+}
+if ( $time == "") {
+	$info [] = 'Nie podano czasu';
+}
+if ( $percentage == "") {
+	$info [] = 'Nie podano oprocentowania';
+}
+
+//nie ma sensu walidować dalej gdy brak parametrów
+if (empty( $info )) {
+	
+	// sprawdzenie, czy $amount, $time i $percentage są liczbami całkowitymi
+	if (! is_numeric( $amount )) {
+		$info [] = 'Pierwsza wartość nie jest liczbą całkowitą';
+	}
+	
+	if (! is_numeric( $time )) {
+		$info [] = 'Druga wartość nie jest liczbą całkowitą';
+	}
+	if (! is_numeric( $percentage )) {
+		$info [] = 'Trzecia wartość nie jest liczbą całkowitą';
+	}	
+
+}
+
+// 3. wykonaj zadanie jeśli wszystko w porządku
+
+if (empty ( $info )) { // gdy brak błędów
+	
+	//konwersja parametrów na int oraz oprocentowania na double
+	$amount = intval($amount);
+	$time = intval($time);
+	$percentage = doubleval($percentage);
+	
+	
+	$q = 1+(($interest/100)/12);
+	$years = $time * 12;
+	//wykonanie operacji;
+	$rata = $sum*pow($q, $years)*($q-1)/((pow($q, $years))-1);
+	$result = round($rata, 2);
+	
+}
+
+// 4. Wywołanie widoku z przekazaniem zmiennych
+// - zainicjowane zmienne ($info,$amount,$time,$percentage,$result)
+//   będą dostępne w dołączonym skrypcie
+include 'calc_view.php';
---
+++ projekt/app/calc_view.php
@@ -0,0 +1,40 @@
+<?php require_once dirname(__FILE__) .'/../config.php';?>
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+<meta charset="utf-8" />
+<title>Kalkulator kredytowy</title>
+</head>
+<body>
+<h1>Witaj w kalkulatorze kredytowym. Wpisz informacje w odpowiednie rubryki by obliczyć swoją rate.</h1>
+<form action="<?php print(_APP_URL);?>/app/calc.php" method="post">
+	<label for="id_amount">Kwota: </label>
+	<input id="id_amount" type="text" name="kwota" value="<?php if(isset($amount))print($amount); ?>" /><br />
+	<label for="id_percentage">Na ile lat: </label>
+	<input id="id_percentage" type="text" name="oprocentowanie" value="<?php if(isset($percentage))print($percentage); ?>" /><br />
+	<label for="id_time">Oprocentowanie(w %): </label>
+	<input id="id_time" type="text" name="czas" value="<?php if(isset($time))print($time); ?>" /><br />
+	<input type="submit" value="Oblicz rate" />
+</form>	
+
+<?php
+//wyświeltenie listy błędów, jeśli istnieją
+if (isset($info)) {
+	if (count ( $info ) > 0) {
+		echo '<ol style="margin: 20px; padding: 10px 10px 10px 30px; border-radius: 5px; background-color: #f88; width:300px;">';
+		foreach ( $info as $key => $msg ) {
+			echo '<li>'.$msg.'</li>';
+		}
+		echo '</ol>';
+	}
+}
+?>
+
+<?php if (isset($result)){ ?>
+<div style="margin: 20px; padding: 10px; border-radius: 5px; background-color: #ff0; width:300px;">
+<?php echo 'Rata miesieczna: '.$result; ?>
+</div>
+<?php } ?>
+
+</body>
+</html>
